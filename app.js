285fff38d90e304b25f586c59c927af17085715c
Anse98 <lorenzo.anselmi98.com>
2023-10-14 01:31:43 +0200

    ADD - Bonus Stilizzato anche html

--- app.js
+++ app.js
@@ -1,43 +1,99 @@
+//////////////////////////////////FUNZIONI/////////////////////////////
 
 
-// Mi vado a prendere il button play dal DOM
-const playBtnElement = document.getElementById("play-btn");
+// FUNZIONE PER GENERARE LA GRIGLIA IN BASE AL LIVELLO DI DIFFICOLTA
+function generateGrill() {
 
-// Mi vado a prendere l'elemento del DOM che contiene le celle
-const gridElement = document.querySelector(".grid");
+  playBtnElement.innerHTML = `Nuova partita <i class="fa-solid fa-rotate-right"></i>` 
 
-
-// Creiamo un evento al click del button
-playBtnElement.addEventListener("click", function (){
+  // Mi vado a prendere l'elemento del DOM che contiene le celle
+  const gridElement = document.querySelector(".grid");
 
   // Cancello la griglia ogni volta che viene premuto il tasto gioca
-  gridElement.innerHTML ="";
+  gridElement.innerHTML = "";
 
-  // Genero 100 volte le caselle
-  for(let i = 0; i < 100; i++) {
-    const n = i + 1;
+  // Mi vado a prendere la select dal DOM
+  const selectElement = document.getElementById("difficulty");
 
-    //Creo la cella che dovrò stampare tot volte nel DOM
-    const cellString = `<div class="cell">${n}</div>`;
+  if (selectElement.value === "f") {
+    // Genero 49 volte le caselle
+    for (let i = 0; i < easy; i++) {
+      const n = i + 1;
 
-    //Concateno ad ogni giro del ciclo la variabile cell
-    gridElement.innerHTML += cellString; 
+      //Creo la cella che dovrò stampare tot volte nel DOM
+      const cellString = `<div class="cell cell-easy">${n}</div>`;
+
+      //Concateno ad ogni giro del ciclo la variabile cell
+      gridElement.innerHTML += cellString;
+    }
+  } else if (selectElement.value === "m") {
+    {
+      // Genero 81 volte le caselle
+      for (let i = 0; i < medium; i++) {
+        const n = i + 1;
+
+        //Creo la cella che dovrò stampare tot volte nel DOM
+        const cellString = `<div class="cell cell-medium">${n}</div>`;
+
+        //Concateno ad ogni giro del ciclo la variabile cell
+        gridElement.innerHTML += cellString;
+      }
+    }
+  } else {
+    // Genero 100 volte le caselle
+    for (let i = 0; i < difficult; i++) {
+      const n = i + 1;
+
+      //Creo la cella che dovrò stampare tot volte nel DOM
+      const cellString = `<div class="cell cell-difficult">${n}</div>`;
+
+      //Concateno ad ogni giro del ciclo la variabile cell
+      gridElement.innerHTML += cellString;
+    }
   }
-    
+}
+
+
+// FUNZIONE PER DARE BG COLOR ALLE CELLE E CONSOLE LOG DEL NUMERO DELLE CELLE
+function addBgAndN () {
+
   // Vado a prendermi tutti gli elementi dal DOM con classe cell
   const cellElements = document.querySelectorAll(".cell");
-  
-  for(let i = 0; i < cellElements.length; i++) {
+
+  for (let i = 0; i < cellElements.length; i++) {
     const n = i + 1;
     const actualCell = cellElements[i];
-    
+
     // Aggiungo l'evento click ad ogni casella
     actualCell.addEventListener("click", function () {
       actualCell.classList.add("bg-azure");
       console.log(n);
     })
   }
-})
+}
+
+  /////////////////////////////FINE FUNZIONI//////////////////////////////////
+
+
+  // Mi creo le variabili che corrispondono al numero di colonne da inserire
+  const easy = 49;
+  const medium = 81;
+  const difficult = 100;
+
+  // Mi vado a prendere il button play dal DOM
+  const playBtnElement = document.getElementById("play-btn");
+
+  // Creiamo un evento al click del button
+  playBtnElement.addEventListener("click", function () {
+
+    generateGrill();
+    
+    addBgAndN ();
+  })
+
+
+
+
 
 
 
